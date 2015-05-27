df16f03d1465e795193c275f00b972f8b6fb21f8
ben-aaron188 <bennettkleinberg@dhcp-29-135.xlate.eduroam.ucl.ac.uk>
2015-05-27 15:15:43 +0100

    centred function

--- core.js
+++ core.js
@@ -114,3 +114,13 @@ function right() {
     left: "+=10"
   })
 }
+
+function css_center(object, relative_object){
+  if (typeof relative_object === "undefined" || relative_object === null) {
+    relative_object = object; 
+  }
+  object.css({
+    "margin-left": -($(relative_object).width() / 2),
+    "margin-top": -($(relative_object).height() / 2)
+  })
+}
